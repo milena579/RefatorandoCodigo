f90458131aee838ab320c929577ff51c04fe30ca
milena579 <milecalegari@gmail.com>
2024-09-13 15:46:35 -0300

    Exercicio OITO

---
+++ exercicioOito.py
@@ -0,0 +1,11 @@
+#https://github.com/Andredev-dias/Base-de-algoritmos-em-C-v3/blob/master/exer12funcoes.c
+def quad (num):
+    quadrado = num * num
+    return quadrado
+
+num = int(input('Informe um numero: '))
+
+res = quad(num)
+
+print(f'O quadrado de {num} é: {res}')
+
